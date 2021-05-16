385fc682c87710930a57afd2cb48d0fbfdbc4644
Wreckss <demandprivacy@icloud.com>
2021-05-15 20:13:04 -0500

    added spot price formatting

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         PriceData priceData = new PriceData();
+        System.out.println(priceData.formattedSpotPrice);
         System.out.println(priceData.formattedSats);
     }
 }
